a62ebd3bd2fbc79ba4ac1c7270a3e72fff0b5611
vadzola <wadzola@yahoo.com.hk>
2022-09-14 14:10:54 +0200

    search options added

---
+++ pages/search.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import SearchHeader from "../components/SearchHeader"
+
+export default function search() {
+
+  return (
+    <div>
+        <Head>
+            <title>Search Page</title>
+        </Head>
+
+        <SearchHeader/>
+    </div>
+  )
+}
